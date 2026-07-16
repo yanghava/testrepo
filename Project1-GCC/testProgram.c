158dce60aabc549ff0bd9a5bdcc64cc64ce50cba
yangh <yangh@avaya.com>
2026-07-16 16:01:25 +0800

    commit the first test program

---
+++ Project1-GCC/testProgram.c
@@ -0,0 +1,10 @@
+#define LOOP 1
+
+int main() {
+    /*below code was generated automatically by VScode*/
+    int i;
+    for (i = 0; i < LOOP; i++) {
+        printf("Hello, World!\n");
+    }
+    return 0;
+}
